ebce4c0d05f5ff3c666e79343907f3207325e72e
yumi-okada <i.rumera.192@gmail.com>
2026-06-08 13:17:42 +0900

    Delete 3-17 directory

--- 3-17/src/Main2.java
+++
@@ -1,9 +0,0 @@
-public class Main2 {
-    public static void main(String[] args) {
-        Student student = new Student("田中太郎", 85);
-        System.out.println(student.getName() + "さんの点数: " + student.getScore() + "点");
-
-        student.setScore(200);
-        student.setName("");
-    }
-}
