0a2e0c128516f951f0467603afc1773d33bd24d0
yumi-okada <i.rumera.192@gmail.com>
2026-07-20 10:49:52 +0900

    Delete 4-22 directory

--- 4-22/src/Employee.java
+++
@@ -1,9 +0,0 @@
-public class Employee {
-    private String name;
-    private int age;
-    private double salary;
-
-    public String getName() { return name; }
-    public int getAge() { return age; }
-    public double getSalary() { return salary; }
-}
